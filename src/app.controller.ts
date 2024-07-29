114e8c12fabdee3298d05e259f3655f33a5b435c
unknown <habibullayevfarzot04@gmail.com>
2024-07-29 12:38:32 +0500

    added stuf

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -25,4 +25,8 @@ export class AppController {
   getAllStuff(): string {
     return `Hello, ${this.appService.getHello()}! Bye, ${this.appService.getBye()}! Contact Information: ${this.appService.getContact()}`;
   }
+  @Get('thankAll')
+  getThankAll(): string {
+    return `Thank you, ${this.appService.getHello()}! Thank you, ${this.appService.getBye()}! Contact Information: ${this.appService.getContact()}`;
+  }
 }
