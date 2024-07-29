2d7b19630b2d988238d6ae4e8ac8613f0aa4f522
unknown <habibullayevfarzot04@gmail.com>
2024-07-29 11:35:50 +0500

    added thanks

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,22 +5,22 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-  @Get('bye')
-  getBye(): string {
-    return this.appService.getBye();
-  }
-  @Get('contact')
-  getContact(): string {
-    return this.appService.getContact();
-  }
-  @Get('allStuff')
-  getAllStuff(): string {
-    return `Hello, ${this.appService.getHello()}! Bye, ${this.appService.getBye()}! Contact Information: ${this.appService.getContact()}`;
-  }
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+  // @Get('bye')
+  // getBye(): string {
+  //   return this.appService.getBye();
+  // }
+  // @Get('contact')
+  // getContact(): string {
+  //   return this.appService.getContact();
+  // }
+  // @Get('allStuff')
+  // getAllStuff(): string {
+  //   return `Hello, ${this.appService.getHello()}! Bye, ${this.appService.getBye()}! Contact Information: ${this.appService.getContact()}`;
+  // }
   @Get('thanks')
   getThanks(): string {
     return 'Thank you for visiting!';
